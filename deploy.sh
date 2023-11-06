1cb03678cc4ddf267d2b2e7a9bc0d0d62c2213ce
vamsi <uvamsi76@gmail.com>
2023-11-06 12:17:54 +0530

    chantges

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.9.0/bin
 
 cd /home/ubuntu/certificate_backend/Backend_Certificate/
 git pull origin main
