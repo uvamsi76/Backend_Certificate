fb9777ba480cfcbd2e42f7949f96445e7dae81dc
vamsi <uvamsi76@gmail.com>
2023-11-06 12:47:21 +0530

    changed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.9.0/bin
+export PATH="$PATH:/home/ubuntu/.nvm/versions/node/v20.9.0/bin/node"
+export NVM_DIR="/home/ubuntu/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 cd /home/ubuntu/certificate_backend/Backend_Certificate/
 git pull origin main
